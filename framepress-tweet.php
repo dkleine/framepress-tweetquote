851ae584bbe6a222a7d22139a4a827369871fdd2
Danny <danny@solutions.io>
2017-03-30 10:26:57 +0200

    Added option to change tweet title

--- framepress-tweet.php
+++ framepress-tweet.php
@@ -1,23 +1,23 @@
 <?php
     /*
     Plugin Name: Framepress QuoteTweet
-    Plugin URI: https://solutions.io/
-    Description: Display a quote that people can tweet.
+    Description: Display a quote that people can tweet using: [fptweet title="$"]$content[/fptweet].
     Author: solutions.io
-    Version: 1.0
+    Version: 1.1
     Author URI: https://solutions.io
     */
 
 function fptweet_shortcode( $atts, $content = null ) {
+  $attr = shortcode_atts(array(
+    'title' => '',
+    ), $atts);
   $link = get_permalink();
-  return '<div class="fptweet">' . $content . '<a target="_blank" href="https://twitter.com/intent/tweet?text=' . $content . '&url=' . $link . '">Klick für Tweet</a></div>';
+  return '<div class="fptweet">' . do_shortcode($content) . '<a target="_blank" href="https://twitter.com/intent/tweet?text=' . do_shortcode($content) . '&url=' . $link . '">'. esc_attr($attr['title']) .'</a></div>';
 }
 add_shortcode( 'fptweet', 'fptweet_shortcode' );
 
 function framepress_tweet_style() {
-    $plugin_url = plugin_dir_url( __FILE__ );
-
-    wp_enqueue_style( 'framepress_tweet_style', $plugin_url . 'assets/css/style.css' );
+  $plugin_url = plugin_dir_url( __FILE__ );
+  wp_enqueue_style( 'framepress_tweet_style', $plugin_url . 'assets/css/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'framepress_tweet_style' );
-?>
